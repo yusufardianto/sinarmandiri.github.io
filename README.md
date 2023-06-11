78322951b2c114694241b3c72bb5c4775e64d042
Muhammad Yusuf Ardianto <56336225+yusufardianto@users.noreply.github.com>
2023-06-11 08:28:09 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# sinarmandiri.github.io
---
+++ vendor/composer/installed.php
@@ -0,0 +1,40 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'twbs/bootstrap' => 
+    array (
+      'pretty_version' => 'v5.3.0',
+      'version' => '5.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '60098ac499d30aa50575b0b7137391c06ef25429',
+    ),
+    'twitter/bootstrap' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v5.3.0',
+      ),
+    ),
+  ),
+);
